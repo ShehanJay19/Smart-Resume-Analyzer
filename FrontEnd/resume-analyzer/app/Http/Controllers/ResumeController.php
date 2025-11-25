fa3d9cb7c3fc3ab951ca0eb35041eb6e2e1d93e3
ShehanJay19 <shehanjaya1921@gmail.com>
2025-11-25 10:05:13 +0530

    front error correction

--- FrontEnd/resume-analyzer/app/Http/Controllers/ResumeController.php
+++ FrontEnd/resume-analyzer/app/Http/Controllers/ResumeController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ResumeController extends Controller
 {
-    //
+    public function analyze(Request $request){
+          $file = $request->file('resume');
+
+          $response = Http::attach(
+            'resume',
+            file_get_contents($file->getRealPath()),
+            $file->getClientOriginalName()
+         )->post('http://127.0.0.1:8000/analyze');
+
+         return view('result',[
+            'text'=>$response['resume_text'],
+            'catogory'=>$response['catogory'],
+
+         ]);
+    }
 }
--- FrontEnd/resume-analyzer/routes/web.php
+++ FrontEnd/resume-analyzer/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ResumeController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('upload');
 });
+
+Route::post('/analyze', [ResumeController::class, 'analyze']);
